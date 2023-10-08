73ba21da48c4ca792210e0a950cbe472b1fff15c
ab <hamdiabdallah681@gmail.com>
2023-10-08 03:39:14 +0100

    use state

--- src/App.js
+++ src/App.js
@@ -3,23 +3,9 @@ import logo from './logo.svg';
 import Products from './components/Products.jsx'
 import './App.css';
 // eslint-disable-next-line no-unused-vars
-import Nextproduct  from './components/nextProduct.jsx'
-const listeTest = [
-  {id : 1 , content1 :"client-side routing and" ,content2 :"non-root public URL by running" },
-  {id : 1 , name : "nizal", content1 :"client-side hhhhhhhdfgd" , content2 : "non-root public URL by running" },
-  {id : 1 , name : "wassim" ,content1 : "client-side and" ,content2 : "the development, run `npm start` or `yarn start" }
-]
-const MyListeTeste = listeTest.map((liss) => {
-  return (<Products content1 = {liss.content1} content2 = {liss.content2} name = {liss.name} /> )
-})
-const myButton = [
-  {a : <i class="fab fa-facebook"><a href="https://www.facebook.com/"> facebook</a></i>},
-  { a : <i class="fab fa-instagram"><a href="https://www.instagram.com/"> instagram</a></i> },
-  { a :<i class="fab fa-x-twitter"><a href="https://www.twitter.com/"> twitter</a></i> }
-]
-const ButtonList = myButton.map((but) => {
-  return (<Nextproduct> {but.a}</Nextproduct> )
-})
+
+import NextProduct from './components/nextProduct.jsx';
+
 function App() {
   return (
     <>
@@ -27,12 +13,7 @@ function App() {
       <h1 className='header'>Pingucoder 🐧</h1>
     </div>
     <div className='dsplay'>
-      <div style={{ height: '100%'}} >
-        {MyListeTeste}
-      </div>
-      <div style={{ height: '100%'}} className={'secondProduct'}>
-        {ButtonList}
-      </div>
+      <NextProduct/>
     </div>
     </>
   );
--- src/components/nextProduct.jsx
+++ src/components/nextProduct.jsx
@@ -1,12 +1,21 @@
 
 
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 function NextProduct({children}) {
+  const [name,setname] = useState("hamdi")
+  function test(){
+    if (name == "hamdi"){
+      setname("abdallah")
+    }else{
+      setname("hamdi")
+    }
+    
+  }
   return (
-    <div style={{margin: '10px 20px'}}>
+    <div style={{margin: '10px 20px',width: '150px'}}>
       
-        <button className='btm'>{children}</button>
+        <button className='btm' onClick={test}>{name}</button>
         
       </div>
     
