410b4844e455964648f66707e2b4907e6d250d63
ab <hamdiabdallah681@gmail.com>
2023-10-14 11:54:55 +0100

    solving the object state inthe form

--- src/components/myInput.jsx
+++ src/components/myInput.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { useState } from 'react'
 
 const MyInput = () => {
-  const [value,setvalue]= useState('');
-  function valueInput(event){
-    setvalue(event.target.value)
-  }
+  const [value,setvalue]= useState({name: "", email: ""});
+  // const [valuee,setvaluee]= useState('');
+  // function valueInput(event){
+  //   setvalue(event.target.value)
+  // }
   return (
-    <div>
+    <form style={{width : '500px',border : 'red 1px'}} >
       <label >Your Name</label>
-      <input value={value} onChange={valueInput} />
-    </div>
+      <input value={value.name} onChange={(event)=> setvalue({...value,name: event.target.value})} /><hr />
+      <label >Email</label>
+      <input value={value.email} onChange={(event)=> setvalue({...value,email: event.target.value})} /> <hr />
+      <button>Submit</button>
+    </form>
   )
 }
 
